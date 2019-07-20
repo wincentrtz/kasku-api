2715fe9f2f4bd780bf45e8dc7203d6b7dff81458
wincentrtz <wincentang3636@gmail.com>
2019-07-20 20:00:18 +0700

    Add JWT Authentication

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,9 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@signup');
+    
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('logout', 'AuthController@logout');
 });
